dde51ec4f50d062189d0b8c223d2936b6765d992
Matt Casperson <mc.casperson@gmail.com>
2022-02-11 09:23:25 -0800

    add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1>Welcome to I'D TAP THAT Taproom!</h1>
+    </div>
+  );
+}
+
+export default Header;
