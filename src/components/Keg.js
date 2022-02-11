f414c0fee4d121cc3f51376798faef20321d13db
Matt Casperson <mc.casperson@gmail.com>
2022-02-11 11:14:30 -0800

    add status message depending on how empty the keg is

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,12 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Keg(props){
+  let kegFullnessMessage = null;
+  if (props.pintsRemaining === 0) {
+    kegFullnessMessage = "Out of stock! Sorry :(";
+  } else if (props.pintsRemaining < 10) {
+    kegFullnessMessage = "Low supply!";
+  }
+
   return (
     <React.Fragment>
       <div onClick = {() => props.whenKegClicked(props.id)}>
         <h3><strong>{props.name}</strong> - from <em>{props.brand}</em></h3>
         <p><em>{props.alcoholContent}% ABV</em> - ${props.price}/pint</p>
         <p>{props.pintsRemaining} pints left!</p>
+        <p>{kegFullnessMessage}</p>
         <hr/>
       </div>
     </React.Fragment>
--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -4,14 +4,20 @@ import PropTypes from "prop-types";
 function KegDetail(props){
   const { keg, onClickingDelete } = props;
   let disabled = false;
-  if (keg.pintsRemaining === 0) disabled = true;
-
+  let kegFullnessMessage = null;
+  if (keg.pintsRemaining === 0) {
+    disabled = true;
+    kegFullnessMessage = "Out of stock! Sorry :(";
+  } else if (keg.pintsRemaining < 10) {
+    kegFullnessMessage = "Low supply!";
+  }
   return (
     <React.Fragment>
       <h1>Keg Detail</h1>
       <h3><strong>{props.keg.name}</strong> - from <em>{keg.brand}</em></h3>
       <p><em>{props.keg.alcoholContent}% ABV</em> - ${keg.price}/pint</p>
       <p>{props.keg.pintsRemaining} pints left!</p>
+      <p>{kegFullnessMessage}</p>
       <button onClick={ props.onClickingPint } className="btn btn-success" disabled={disabled}>Pour me a glass!</button>
       <button onClick={ props.onClickingEdit } className="btn btn-warning" disabled={disabled}>Edit this keg!</button>
       <button onClick={()=> onClickingDelete(keg.id) } className="btn btn-danger">Throw this keg out!</button>
