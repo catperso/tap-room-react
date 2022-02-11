0dc19230a65281fdde2cb74dfca4a1ef4fe923e2
Matt Casperson <mc.casperson@gmail.com>
2022-02-11 11:37:52 -0800

    add keg.png, adjust color as keg gets low

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,17 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import kegPic from "./../img/keg.png";
 
 function Keg(props){
   let kegFullnessMessage = null;
+  let kegPicClass = "kegIcon"
   if (props.pintsRemaining === 0) {
     kegFullnessMessage = "Out of stock! Sorry :(";
+    kegPicClass = "kegIcon kegEmpty";
   } else if (props.pintsRemaining < 10) {
     kegFullnessMessage = "Low supply!";
+    kegPicClass = "kegIcon kegLow";
   }
 
   return (
     <React.Fragment>
       <div onClick = {() => props.whenKegClicked(props.id)}>
+        <img src={kegPic} alt="The same pic of a keg" className={kegPicClass} />
         <h3><strong>{props.name}</strong> - from <em>{props.brand}</em></h3>
         <p><em>{props.alcoholContent}% ABV</em> - ${props.price}/pint</p>
         <p>{props.pintsRemaining} pints left!</p>
