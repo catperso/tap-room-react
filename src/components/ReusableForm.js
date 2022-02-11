1a93de2f35f54392e763cf1371de3db8b706b355
Matt Casperson <mc.casperson@gmail.com>
2022-02-11 12:48:56 -0800

    add kegtap.jpg and adjust styling on reusable form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,28 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
+import kegTapPic from "./../img/kegtap.jpg";
 
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Name Of Brew' />
-        <input
-          type='text'
-          name='brand'
-          placeholder='Brewery' />
-        <input
-          type='number'
-          name='price'
-          placeholder='Price per pint' />
-        <input
-          type='number'
-          name='alcoholContent'
-          placeholder='%ABV' />
-        <button type='submit' className="btn btn-success">{props.buttonText}</button>
-      </form>
+      <div className="row">
+        <h3 className="tapListHeader">Tap a new brew!</h3>
+        <div className="col-6">
+          <form onSubmit={props.formSubmissionHandler} className="form-control">
+            <label for='name' className="form-label">Brew Name</label>
+            <input
+              className="form-control"
+              type='text'
+              name='name'
+              placeholder='Name Of Brew' />
+            <label for='brand' className='form-label'>Brewery Name</label>
+            <input
+              className="form-control"
+              type='text'
+              name='brand'
+              placeholder='Brewery' />
+            <label for="price" className="form-label">Price per Pint</label>
+            <input
+              className="form-control"
+              type='number'
+              name='price'
+              min="0"
+              placeholder='$$$' />
+            <label for="alcoholContent" className="form-label">Alcohol Percentage</label>
+            <input
+              className="form-control"
+              type='number'
+              name='alcoholContent'
+              placeholder='%ABV' />
+            <br/>
+            <button type='submit' className="btn btn-success">{props.buttonText}</button>
+          </form>
+        </div>
+        <div className="col-6">
+          <img src={kegTapPic} alt="A man tapping a keg" className="kegTapping" />
+        </div>
+      </div>
+      <br/>
     </React.Fragment>
   );
 }
